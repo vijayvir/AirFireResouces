6bcb7226b1e52e1d5a9841f6913bd9b78e3cfe98
jaismeenk110 <130955005+jaismeenk110@users.noreply.github.com>
2023-05-21 12:09:23 +0530

    Responsive Ant Fire Labs

--- parallax-Effect/Parallax-Effect.js
+++ parallax-Effect/Parallax-Effect.js
@@ -15,12 +15,3 @@
 
 
 
-
-var counter=1;
-setInterval(function(){
-    document.getElementById('radio'+counter).checked=true;
-    counter++;
-    if(counter>4){
-        counter=2;
-    }
-},5000);
